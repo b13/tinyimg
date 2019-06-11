80c72b8362d0ac520d58b131e490840406900bd9
David Steeb <david.steeb@b13.de>
2019-06-11 12:24:07 +0200

    [BUGFIX] Fix compressImages to allow for paths with space characters

--- Classes/Command/CompressImagesCommandController.php
+++ Classes/Command/CompressImagesCommandController.php
@@ -77,7 +77,7 @@ class CompressImagesCommandController extends CommandController
         foreach ($files as $file) {
             if ($file instanceof \Schmitzal\Tinyimg\Domain\Model\File) {
                 $file = $this->resourceFactory->getFileObject($file->getUid());
-                if (filesize(GeneralUtility::getFileAbsFileName($file->getPublicUrl())) > 0) {
+                if (filesize(urldecode(GeneralUtility::getFileAbsFileName($file->getPublicUrl()))) > 0) {
                     $this->compressImageService->initializeCompression($file);
                     $fileDeletionAspect->cleanupProcessedFilesPostFileReplace($file, '');
                 }
